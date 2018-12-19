baa71876d2c36e5b436aef6579949c44eeb280a4
Conrado Cruz <conrado27cym@gmail.com>
2018-12-19 09:57:21 -0600

    Mejorado pero con deficiencias en lo gráfico

--- Josephus.java
+++ Josephus.java
@@ -3,88 +3,75 @@
  *  Execution:    java Josephus m n
  *  Dependencies: Queue.java
  *                StDraw.java
- *
  *  % java Josephus 2 7
  *  2 4 6 1 5 3 7
  ******************************************************************************/
 import java.awt.Font;
 import java.lang.reflect.*;
-
 public  class Josephus{
-  static double corx[];
-  static double cory[];
-  public  void calcularCoordenadas(int p){   
-    StdDraw.setCanvasSize(650, 650);
-    Font font = new Font("Eras Light ITC", Font.BOLD, 14);
-    StdDraw.setFont(font);
-    StdDraw.setPenRadius(0.04);
-    corx= new double [p];
-    cory= new double [p];
-    double x=0.5;
-    double y=0.5;
-    for(int c=0; c<p; c++){
-      double a = (360/p*c); 
-      double convert = (double) Math.toRadians(a) ;
-      x = 0.43*Math.cos(convert)+0.5;
-      y = 0.43*Math.sin(convert)+0.5;
-      StdDraw.point(x,y);
+  static double corx[],cory[];
+  private void calcularCoordenadas(int personas){   
+    corx= new double [personas];
+    cory= new double [personas];
+    double x,y,origen =0.5;
+    double radio      =0.3;
+    for(int c=0; c<personas; c++){
+      double grados = (360/personas*c); 
+      double convert = (double) Math.toRadians(grados) ;
+      x=radio*Math.cos(convert)+origen;
+      y=radio*Math.sin(convert)+origen;
       corx[c]=x;
       cory[c]=y;
     }
   }
+  
+  private static void dibujarPuntos(){    
+    StdDraw.setPenRadius(0.04);
+    for(int g=0 ; g<corx.length; g++){
+      StdDraw.point(corx[g],cory[g]);  
+    }
+  }
 
-  public void iniciarMatanza (int n, int m) {  
+  public void play (int n, int m) {  
     Queue<Integer> queue = new Queue<Integer>(); 
-    int numero =0;
-    int count=0;
-    for (int c = 1; c <= n; c++)
+    int eliminado,count =0;
+    //masinfo(count,m,eliminado);
+    for (int c = 1; c <= n; c++){
       queue.enqueue(c);
-    
-    while (!queue.isEmpty()) {
+    }
+    while (!queue.isEmpty()){
       count ++;
-      for (int c = 0; c < m-1; c++)
+      for (int c = 1; c < m; c++)
       queue.enqueue(queue.dequeue());
-      numero = queue.dequeue(); 
-      if(queue.size()>0 ) { masinfo(count,m,numero); matar(numero);} 
+      eliminado = queue.dequeue(); 
+      masinfo(count,eliminado);
+      eliminar(eliminado-1);
     } 
   }
   
-  private static void matar(int numero){
-    Font font1 = new Font("Eras Light ITC", Font.BOLD, 14);
-    StdDraw.setFont(font1);
-    for(int c =1;c <=corx.length; c++){
-      if(c==numero){
-        StdDraw.setPenColor(StdDraw.WHITE);
-        StdDraw.point(corx[c-1],cory[c-1]);
-        StdDraw.setPenColor(StdDraw.RED);
-        StdDraw.text(corx[c-1],cory[c-1],""+c); 
-        StdDraw.pause(1000);
-      }
-    }
+  private static void eliminar(int eliminado){
+    Font font = new Font("Eras Light ITC", Font.BOLD, 14);
+    StdDraw.setFont(font);   
+    StdDraw.setPenRadius(0.05);
+    
+    StdDraw.setPenColor(StdDraw.WHITE);
+    StdDraw.point(corx[eliminado],cory[eliminado]);
+    StdDraw.setPenColor(StdDraw.RED);
+    StdDraw.text(corx[eliminado],cory[eliminado],""+eliminado); 
+    StdDraw.pause(1000);
   } 
   
-  private static void masinfo(int c,int num, int m){ 
+  private static void masinfo(int c, int m){ 
     StdDraw.setPenColor(StdDraw.BLUE);
-    if(c<25){
-      StdDraw.text(c*0.04,0.985,m+"");
-    }
-    else if(c>=25 && c<49){
-      StdDraw.text((c-24)*0.04,0.985-0.02,m+"");
-    }
-    else if(c>=49 && c<73){
-      StdDraw.text((c-48)*0.04,0.985-0.04,m+"");
-    }
-    else {
-      StdDraw.text((c-72)*0.04,0.985-0.06,m+"");
-    }
+    StdDraw.text(c*0.04,0.985,m+"");
   }
-  
   
   public static void main(String[] args){
     Josephus j = new Josephus();
-    int n = Integer.parseInt(args[0]); //n = vueltas
-    int m = Integer.parseInt(args[1]); //m = personas
+    int n = 2; ///Integer.parseInt(args[0]); //n = vueltas
+    int m = 10;//Integer.parseInt(args[1]); //m = personas
     j.calcularCoordenadas(m);
-    j.iniciarMatanza(m,n);
+    j.dibujarPuntos();
+    j.play(m,n);
   }
 }
